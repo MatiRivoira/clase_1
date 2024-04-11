afb7f3461d255cef4e9c86c8b4e7f4e2233b0920
MatiRivoira <98726423+MatiRivoira@users.noreply.github.com>
2024-04-11 11:44:24 -0300

    Laboratorio IV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase_1';
+  protected edadUno:string = "";
+  protected edadDos:string = "";
+  protected calcularPromedio() {
+    alert(`El promedio es ${(parseInt(this.edadUno) + parseInt(this.edadDos)) / 2}`);
+  };
+  protected limpiarCuadros(){
+    this.edadUno = "";
+    this.edadDos = "";
+  };
 }
+
+export class Usuario {
+  protected nombre:string = "";
+  protected clave:string = "";
+}
+
